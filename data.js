ee9aed966bacfb33d8406ae0146e9f749f2a8eab
摳得柴柴 <87404598+coder220224@users.noreply.github.com>
2025-07-02 09:05:30 +0800

    Update available areas

--- data.js
+++ data.js
@@ -1,1 +1,1 @@
-export const availableAreas = {"available_areas": ["D2區3800", "C4區4800"], "timestamp": 1751418323};
+export const availableAreas = {"available_areas": ["D2區3800", "B3區5800"], "timestamp": 1751418329};
